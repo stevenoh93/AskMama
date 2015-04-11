147533f58f9486ac2ad0431bfe2100228cf890b0
StevenVaio <stevenoh93@gmail.com>
2015-04-11 15:13:10 -0400

    ViewPager not working

--- mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/RegisterFragment.java
+++ mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/RegisterFragment.java
@@ -2,23 +2,66 @@ package com.stevenoh.reginahong.askmama;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterFragment extends Fragment {
+    public static final String EXTRA_INFO_PAGE = "askmama.page";
+    private static final String TAG = "RegisterFragmet";
+    private int mPage = 0;
+
+    public static RegisterFragment newInstance(int idx) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_INFO_PAGE, idx);
+
+        RegisterFragment fragment = new RegisterFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);
 
+        if (getArguments() != null)
+            mPage = getArguments().getInt(EXTRA_INFO_PAGE);
+
+        setRetainInstance(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_register_info, parent, false);
 
+        ImageView logo = (ImageView) v.findViewById(R.id.logo_image);
+        TextView textView = (TextView) v.findViewById(R.id.register_textview);
+        Button leftCircle = (Button) v.findViewById(R.id.progress_circle1);
+        Button rightCircle = (Button) v.findViewById(R.id.progress_circle2);
+        switch (mPage) {
+            case 0:
+                Log.d(TAG, "Case 0 selected");
+                logo.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.GONE);
+                leftCircle.setBackgroundResource(R.drawable.progress_circle_selected);
+                rightCircle.setBackgroundResource(R.drawable.progress_circle);
+                break;
+            case 1:
+                Log.d(TAG, "Case 1 selected");
+                logo.setVisibility(View.GONE);
+                textView.setVisibility(View.VISIBLE);
+                leftCircle.setBackgroundResource(R.drawable.progress_circle);
+                rightCircle.setBackgroundResource(R.drawable.progress_circle_selected);
+                break;
+        }
+
         return v;
     }
+
+
 }
--- mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/RegisterPagerActivity.java
+++ mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/RegisterPagerActivity.java
@@ -1,13 +1,19 @@
 package com.stevenoh.reginahong.askmama;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import java.util.ArrayList;
 
 public class RegisterPagerActivity extends FragmentActivity {
+    private static final String TAG = "RegisterPagerActivity";
     private ViewPager mViewPager;
+
     private RegisterInfoPage[] mInfos = new RegisterInfoPage[] {
             new RegisterInfoPage(-1, R.drawable.logo),
             new RegisterInfoPage(R.string.register_info_2, -1)
@@ -17,10 +23,20 @@ public class RegisterPagerActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mViewPager = new ViewPager(this);
-        mViewPager.setId(R.id.viewPager);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
         setContentView(mViewPager);
 
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int i) {
+                return RegisterFragment.newInstance(i);
+            }
+            @Override
+            public int getCount() { return mInfos.length; }
+        });
 
+
+        mViewPager.setCurrentItem(0);
     }
 }
