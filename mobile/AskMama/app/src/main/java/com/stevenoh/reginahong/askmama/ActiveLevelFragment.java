33f4b899de7761df6c7bb9f123e8ad773fdd7c64
Steven Desktop <stevenoh93@gmail.com>
2015-05-04 00:57:47 -0400

    Images replaced and click feel changes

--- mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/ActiveLevelFragment.java
+++ mobile/AskMama/app/src/main/java/com/stevenoh/reginahong/askmama/ActiveLevelFragment.java
@@ -18,6 +18,7 @@ public class ActiveLevelFragment extends Fragment {
     private static final String TAG = "ActiveLevelFragment";
     private int mPage = 5;
     private UserProfile mUser = UserProfile.get();
+    private LinearLayout levels;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,42 +30,45 @@ public class ActiveLevelFragment extends Fragment {
         super.onCreateView(inflater,parent,savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_active_level_page, parent, false);
 
-        LinearLayout levels = (LinearLayout) v.findViewById(R.id.active_level_choice_list);
+        levels = (LinearLayout) v.findViewById(R.id.active_level_choice_list);
         TextView curTextView;
         curTextView = (TextView) levels.getChildAt(0);
-
+        curTextView.setBackgroundResource(R.color.active01);
         curTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mUser.setActiveLevel(0);
-                moveToNextPage();
+                moveToNextPage(0);
             }
         });
 
         curTextView = (TextView) levels.getChildAt(1);
+        curTextView.setBackgroundResource(R.color.active02);
         curTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mUser.setActiveLevel(1);
-                moveToNextPage();
+                moveToNextPage(1);
             }
         });
 
         curTextView = (TextView) levels.getChildAt(2);
+        curTextView.setBackgroundResource(R.color.active03);
         curTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mUser.setActiveLevel(2);
-                moveToNextPage();
+                moveToNextPage(2);
             }
         });
 
         curTextView = (TextView) levels.getChildAt(3);
+        curTextView.setBackgroundResource(R.color.green);
         curTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mUser.setActiveLevel(3);
-                moveToNextPage();
+                moveToNextPage(3);
             }
         });
 
@@ -76,9 +80,10 @@ public class ActiveLevelFragment extends Fragment {
         return true;
     }
 
-    private void moveToNextPage() {
+    private void moveToNextPage(int i) {
         if (validateInput()) {
             // Change clicked item color
+            levels.getChildAt(i).setBackgroundResource(R.color.dark_green);
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
